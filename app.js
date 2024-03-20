5dd570630d3543075ab8f5f3d2640023d408bd2b
ViacheslavSosnovskiy <saintstill333@gmail.com>
2024-03-20 17:47:17 +0200

    delete morgan

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
-const logger = require("morgan")
 
 const app = express()
 
-const formatsLogger = app.get("env") === "devalopment" ? "dev" : "short"
-
 const authRouter = require("./routes/api/auth")
 
-app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
